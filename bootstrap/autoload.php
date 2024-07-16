efef79190b7816cfccb7d4102ab008b12f06a9b2
Ngo The Duong <duong@duongngo.com>
2024-07-16 09:35:19 +0700

    Update autoload.php
    update

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -7,10 +7,7 @@ define('LARAVEL_START', microtime(true));
 | Register The Composer Auto Loader
 |--------------------------------------------------------------------------
 |
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually". Feels great to relax.
+Update Bootstrap
 |
 */
 // require __DIR__ . '/../app/Library/array_function.php';
@@ -22,10 +19,7 @@ require __DIR__.'/../vendor/autoload.php';
 |--------------------------------------------------------------------------
 | Include The Compiled Class File
 |--------------------------------------------------------------------------
-|
-| To dramatically increase your application's performance, you may use a
-| compiled class file which contains all of the classes commonly used
-| by a request. The Artisan "optimize" is used to create this file.
+
 |
 */
 
