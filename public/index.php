a448940beedd0f9f303bbbedea8cbe4c9d851d42
Ngo The Duong <duong@duongngo.com>
2024-11-12 08:16:16 +0700

    Update index.php
    update ver

--- public/index.php
+++ public/index.php
@@ -9,13 +9,13 @@
 
 /*
 |--------------------------------------------------------------------------
-| Register The Auto Loader
+| Register The Auto Loader v2.0
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader for
 | our application. We just need to utilize it! We'll simply require it
 | into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels nice to relax.
+| loading any of our classes later on. It feels nice to relax. Thanks!
 |
 */
 
